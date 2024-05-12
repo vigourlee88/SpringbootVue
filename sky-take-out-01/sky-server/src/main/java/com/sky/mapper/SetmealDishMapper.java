e3951f61b81d1a261b2e94e3938ab24507270fc3
itcast <vigourlee137@gmail.com>
2024-05-12 13:38:33 +0900

    缓存套餐代码开发

--- sky-take-out-01/sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
+++ sky-take-out-01/sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
@@ -1,5 +1,7 @@
 package com.sky.mapper;
 
+import com.sky.entity.SetmealDish;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -16,4 +18,18 @@ public interface SetmealDishMapper {
     //select setmeal_id from setmeal_id where dish_id IN #{1,2,3,4...} 动态拼接菜品id List集合
     List<Long> getSetmealIdsByDishIds(List<Long> dishIds);
 
+    /**
+     * 批量保存套餐和菜品的关联关系
+     *
+     * @param setmealDishes
+     */
+    void insertBatch(List<SetmealDish> setmealDishes);
+
+    /**
+     * 根据套餐id删除套餐和菜品的关联关系
+     *
+     * @param setmealId
+     */
+    @Delete("delete from setmeal_dish where setmeal_id = #{setmealId}")
+    void deleteBySetmealId(Long setmealId);
 }
